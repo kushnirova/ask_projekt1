ac9ce469b7a513d2ff947c1ad019a485011b9839
kushnirova <21267ans@gmail.com>
2024-12-12 21:54:17 +0100

    Add files via upload
    Dodałam funkcję, która wybiera typ zmiennej dla druku binarnego/bajtowego i sam druk binarny i bajtowy.

--- projekt1ask.c
+++ projekt1ask.c
@@ -34,9 +34,82 @@ void menu(){
 }
 
 int wyboropcji(){
+    int opcja, k;
+    printf(" 1. Int");
+    printf("\n 2. Float");
+    printf("\n 3. Double \n");
+
+    do {
+        printf("\n Podaj typ liczby: ");
+        k=scanf("%d",&opcja);
+        if(k==0||opcja<1||opcja>3) printf(" Przy wyborze opcji wystapil blad, sprobuj ponownie");
+        while (getchar()!='\n');
+    }
+    while(k==0||opcja<1||opcja>3);
+    system("cls");
+    printf("\n");
+    return opcja;
 }
 
-void wczytdrukbin(){
+void wczytdrukbin(int typ){
+
+    int liczbaINT, k;
+    float liczbaFLOAT;
+    double liczbaDOUBLE;
+
+    unsigned char *bajtINT = (unsigned char*)&liczbaINT;
+    unsigned char *bajtFLOAT = (unsigned char*)&liczbaFLOAT;
+    unsigned char *bajtDOUBLE = (unsigned char*)&liczbaDOUBLE;
+
+    if(typ==1){
+        do {
+            printf("\n Podaj liczbe: ");
+            k=scanf("%d",&liczbaINT);
+            if (k==0) printf("\n Blad formatu, sprobuj ponownie");
+            while(getchar()!='\n');
+        }
+        while(k==0);
+        printf(" Liczba %d w postaci binarnej: ", liczbaINT);
+
+        for(int i=sizeof(liczbaINT)-1; i>=0;i--){
+            drukbin(bajtINT[i]);
+            printf(" ");
+        }
+        printf("\n");
+
+    }
+    if(typ==2){
+        do {
+            printf("\n Podaj liczbe: ");
+            k=scanf("%f",&liczbaFLOAT);
+            if (k==0) printf("\n Blad formatu, sprobuj ponownie");
+            while(getchar()!='\n');
+        }
+        while(k==0);
+        printf(" Liczba %f w postaci binarnej: ", liczbaFLOAT);
+        for(int i=sizeof(liczbaFLOAT)-1; i>=0;i--){
+            drukbin(bajtFLOAT[i]);
+            printf(" ");
+        }
+        printf("\n");
+    }
+    if(typ==3){
+        do {
+            printf("\n Podaj liczbe: ");
+            k=scanf("%lf",&liczbaDOUBLE);
+            if (k==0) printf("\n Blad formatu, sprobuj ponownie");
+            while(getchar()!='\n');
+        }
+        while(k==0);
+        printf(" Liczba %lf w postaci binarnej: ", liczbaDOUBLE);
+        for(int i=sizeof(liczbaDOUBLE)-1; i>=0;i--){
+            drukbin(bajtDOUBLE[i]);
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+
 }
 
 void drukbin(unsigned char bajt){
@@ -45,16 +118,69 @@ void drukbin(unsigned char bajt){
     }
 }
 
-void wczytdrukbajt(){
+void wczytdrukbajt(int typ){
+
+    int liczbaINT, k;
+    float liczbaFLOAT;
+    double liczbaDOUBLE;
+
+    unsigned char *bajtINT = (unsigned char*)&liczbaINT;
+    unsigned char *bajtFLOAT = (unsigned char*)&liczbaFLOAT;
+    unsigned char *bajtDOUBLE = (unsigned char*)&liczbaDOUBLE;
+
+    if(typ==1){
+        do {
+            printf("\n Podaj liczbe: ");
+            k=scanf("%d",&liczbaINT);
+            if (k==0) printf("\n Blad formatu, sprobuj ponownie");
+            while(getchar()!='\n');
+        }
+        while(k==0);
+        printf(" Liczba %d w postaci bajtowej: ", liczbaINT);
+
+        for(int i=sizeof(liczbaINT)-1; i>=0;i--){
+            printf("%02x ",bajtINT[i]);
+        }
+        printf("\n");
+
+    }
+    if(typ==2){
+        do {
+            printf("\n Podaj liczbe: ");
+            k=scanf("%f",&liczbaFLOAT);
+            if (k==0) printf("\n Blad formatu, sprobuj ponownie");
+            while(getchar()!='\n');
+        }
+        while(k==0);
+
+        printf(" Liczba %f w postaci bajtowej: ", liczbaFLOAT);
+        for(int i=sizeof(liczbaFLOAT)-1; i>=0;i--){
+            printf("%02x ",bajtFLOAT[i]);
+        }
+        printf("\n");
+    }
+    if(typ==3){
+        do {
+            printf("\n Podaj liczbe: ");
+            k=scanf("%lf",&liczbaDOUBLE);
+            if (k==0) printf("\n Blad formatu, sprobuj ponownie");
+            while(getchar()!='\n');
+        }
+        while(k==0);
+        printf(" Liczba %lf w postaci bajtowej: ", liczbaDOUBLE);
+        for(int i=sizeof(liczbaDOUBLE)-1; i>=0;i--){
+            printf("%02x ", bajtDOUBLE[i]);
+        }
+        printf("\n");
+    }
+
+
 }
 
 void odejmowanieU2(){
 }
 
 void konwersja(){
-}
-
-void drukbainarny(){
 }
 
 int main(){
